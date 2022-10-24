494781743ef9b2b408919af3bf8c3371d2c422bc
José Juan Téllez Guzmán <jjtellezg@gmail.com>
2022-10-24 11:31:20 -0500

    logout funtion

--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from django.contrib.auth import login
+from django.contrib.auth import login, logout
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.db import IntegrityError
@@ -39,3 +39,8 @@ def signup(request):
 
 def tasks(request):
     return render(request, 'tasks/tasks.html')
+
+
+def signout(request):
+    logout(request)
+    return redirect('home')
