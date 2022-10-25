7c163b48103f0d88a78c6c6153ea3a3d83442e90
José Juan Téllez Guzmán <jjtellezg@gmail.com>
2022-10-25 16:57:37 -0500

    actualizar tareas

--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
@@ -59,6 +59,21 @@ def create_task(request):
                 'form': AuthenticationForm,
                 'error': "Please provide valide data"
             })
+
+
+def task_detail(request, task_id):
+    if request.method == 'GET':
+        task = get_object_or_404(Task, pk=task_id, user=request.user)
+        form = TaskForm(instance=task)
+        return render(request, 'tasks/task_detail.html', {'task': task, 'form': form})
+    else:
+        try:
+            task = get_object_or_404(Task, pk=task_id, user=request.user)
+            form = TaskForm(request.POST, instance=task)
+            form.save()
+            return redirect('tasks')
+        except ValueError:
+            return render(request, 'tasks/task_detail.html', {'task': task, 'form': form, 'error': "Error updating task"})
 
 
 def signout(request):
