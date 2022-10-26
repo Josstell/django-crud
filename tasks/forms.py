f5f73fbadf4017c08d9294f6d81349ca8aefc5c2
José Juan Téllez Guzmán <jjtellezg@gmail.com>
2022-10-26 11:45:11 -0500

    Bootstap

--- tasks/forms.py
+++ tasks/forms.py
@@ -1,8 +1,15 @@
+from django import forms
 from django.forms import ModelForm
 from .models import Task
 
 
-class TaskForm(ModelForm):
+class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
         fields = ['title', 'description', 'important']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'description': forms.Textarea(attrs={'class': 'form-control'}),
+            'important': forms.CheckboxInput(attrs={'class': 'form-check-input m-auto'})
+
+        }
